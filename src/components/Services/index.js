27f9846fd09fcf9cbcc5b7f5ee8aec75f18ba8c5
nah-nova <noaheutz@hotmil.nl>
2021-04-26 12:05:59 +0200

    Services section finished.

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import Icon1 from '../../images/svg-4.svg'
+import Icon2 from '../../images/svg-5.svg'
+import Icon3 from '../../images/svg-6.svg'
+import { 
+    ServicesContainer, 
+    ServicesH1, 
+    ServicesCard, 
+    ServicesIcon, 
+    ServicesWrapper, 
+    ServicesH2, 
+    ServicesP
+} from './ServicesElements'
 
 const Services = () => {
     return (
         <ServicesContainer id="services">
-            
+            <ServicesH1>Our Services</ServicesH1>
+                <ServicesWrapper>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon1}/>
+                        <ServicesH2>Reduce exspenses</ServicesH2>
+                        <ServicesP>We help reduce your fees and increase your overall revenue.</ServicesP>
+                    </ServicesCard>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon2}/>
+                        <ServicesH2>Virtual Offices</ServicesH2>
+                        <ServicesP>This platform can be used anywhere in the world that accepts our bank.</ServicesP>
+                    </ServicesCard>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon3}/>
+                        <ServicesH2>Premium benefits</ServicesH2>
+                        <ServicesP>Unlock our special membership for 5% of every purchase.</ServicesP>
+                    </ServicesCard>
+                </ServicesWrapper>
         </ServicesContainer>
     )
 }
